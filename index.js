217c585d8a421b0a32c57acbfff9300bcffb4825
zhuaq <zhuaq@7000mall.com>
2022-04-02 17:32:29 +0800

    app.ts

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @Description: xingp，yyds
  * @Author: zaq
  * @Date: 2022-04-02 17:25:32
- * @LastEditTime: 2022-04-02 17:25:33
+ * @LastEditTime: 2022-04-02 17:32:15
  * @LastEditors: zaq
  * @Reference: 
  */
-require('./app.js')
+require('./app.ts')
