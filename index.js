d1ca76cf15c364acfca7c2036d2742a875b0c288
zhuaq <zhuaq@7000mall.com>
2022-04-02 17:26:14 +0800

    vercel配置文件

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: xingp，yyds
+ * @Author: zaq
+ * @Date: 2022-04-02 17:25:32
+ * @LastEditTime: 2022-04-02 17:25:33
+ * @LastEditors: zaq
+ * @Reference: 
+ */
+require('./app.js')
