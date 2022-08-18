5dcfeedd6329cd83e7fe71ab8fd49407951ae7c5
zhuaq <zhuaq@7000mall.com>
2022-08-18 17:44:07 +0800

    暂存 测试

--- app.ts
+++ app.ts
@@ -80,4 +80,3 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
--- index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Description: xingp，yyds
- * @Author: zaq
- * @Date: 2022-04-02 17:25:32
- * @LastEditTime: 2022-04-02 17:32:15
- * @LastEditors: zaq
- * @Reference: 
- */
-require('./app.ts')
